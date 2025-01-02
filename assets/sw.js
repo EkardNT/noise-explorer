cc742f77950d9f3d8b8ffb266e662558b6b4e123
Drake Tetreault <ekardnt@ekardnt.com>
2025-01-01 16:31:42 -0800

    Update template.

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './noise_explorer.js',
+  './noise_explorer_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
